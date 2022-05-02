268ebe205c5c82a5fc85d5e4f6a8cc4ba5aa9e14
Alex Contreras <alex.code29@gmail.com>
2022-05-02 03:04:55 -0500

    added api route to read the db.json file

--- routes/apiRoutes/note.js
+++ routes/apiRoutes/note.js
@@ -1,4 +1,11 @@
 const router = require('express').Router();
 const db = require('../../db/db.json');
+const newNote = require('../../lib/note')
+
+router.get('/notes', (req, res) => {
+    let results = db
+    return res.json(results)
+});
 
 
+module.exports = router
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3001
 const app = express();
 const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes/note')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.use('/', htmlRoutes);
+app.use('/api', apiRoutes)
 
 app.listen(PORT, () => {
     console.log(`Now listening on port: ${PORT}`)
